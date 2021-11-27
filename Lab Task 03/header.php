975e2c17b57b82e6523de335694820c11a863029
Md Abu Sufian <63811452+mdabusufian@users.noreply.github.com>
2021-11-28 01:00:01 +0600

    Add files via upload

---
+++ Lab Task 03/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+    <div>
+        <!-- Starting of Navigation Bar -->
+                <a href="Form iO 03.php" class="option">Registration</a>
+            </span>&nbsp;|&nbsp;
+            <span>&nbsp;</span>
+                <a href="login.php" class="option">Login</a>
+            </span>
+            <span>&nbsp;|&nbsp;</span>
+            <span>&nbsp;</span>
+        </navbar>
+        <!-- Ending of Navigation Bar -->
+    </div>
+</body>
+</html>
